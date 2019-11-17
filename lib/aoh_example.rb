fc57338dbf3db14f5b73c14e8b17384bb16d0991
dwane richards <dwanerich@gmail.com>
2019-11-17 19:51:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,6 +10,9 @@ def assembled_aoh
   
   new_hash = [
     {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate Gompert", :occupation => "None"},
     {:name => "Bruce Green", :occupation => "Fan of Mildred"}
   ]
   
