1f78d3371492a0f39d93dd8b7151b63f6855fa2a
dwane richards <dwanerich@gmail.com>
2019-11-17 20:27:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,9 +23,8 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  p [2][:name]
-    
-    
+  new_array =[
+    [2][:name]
     ]
  
  
